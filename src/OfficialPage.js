f33cc2ecff8dab3d82926f4c4fe7c99c19350c79
Devin Dai <wdai@ualberta.ca>
2021-05-13 23:33:09 -0600

    Updated Official Page

--- src/OfficialPage.js
+++ src/OfficialPage.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
 import red from '@material-ui/core/colors/red';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-
 import AppBar from './components/AppBar';
 import homelogo from './Images/TF_Icon.jpg'
 import Toolbar, { styles as toolbarStyles } from './components/Toolbar';
+import { Carousel } from 'antd';
+import 'antd/dist/antd.css';
+import Earth from './Images/earth.jpg' 
 import {
   THIRD_PARTY_URL,
   OFFICIAL_URL,
@@ -88,20 +76,22 @@ const styles = theme => (
   }
 );
 
+
+
 class SimpleCard extends React.Component {
-
-  state = { expanded: false };
-
-  handleExpandClick = () => {
-    this.setState({ expanded: !this.state.expanded });
-  };
-
   render () {
     const { classes } = this.props;
-    const bull = <span className={classes.bullet}>•</span>;
+
+    const contentStyle = {
+      height: '897px',
+      color: '#fff',
+      lineHeight: '160px',
+      textAlign: 'center',
+      backgroundImage: `url(${Earth})`,
+    };
 
     return (
-      <div className={classes.root}>
+      <div>
         <AppBar position="fixed">
           <Toolbar className={classes.toolbar}>
             <div className={classes.left} style={{backgroundColor:'black'}}/>
@@ -114,117 +104,21 @@ class SimpleCard extends React.Component {
               <Button color="inherit" href={GOKIN_URL}>Gokin</Button>
           </Toolbar>
         </AppBar>
-        <div className={classes.placeholder} />
-        <Card className={classes.card}>
-          <CardContent>
-            <Typography className={classes.title} color="textSecondary">
-              Word of the Day
-            </Typography>
-            <Typography variant="headline" component="h2">
-              be{bull}nev{bull}o{bull}lent
-            </Typography>
-            <Typography className={classes.pos} color="textSecondary">
-              adjective
-            </Typography>
-            <Typography component="p">
-              well meaning and kindly.<br />
-              {'"a benevolent smile"'}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Learn More</Button>
-          </CardActions>
-        </Card>
-  
-        <Card className={classes.card}>
-          <CardMedia
-            className={classes.media}
-            image="https://material-ui.com/static/images/cards/contemplative-reptile.jpg"
-            title="Contemplative Reptile"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="headline" component="h2">
-              Lizard
-            </Typography>
-            <Typography component="p">
-              Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-              across all continents except Antarctica
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small" color="primary">
-              Share
-            </Button>
-            <Button size="small" color="primary">
-              Learn More
-            </Button>
-          </CardActions>
-        </Card>
-  
-        <Card className={classes.card}>
-            <CardHeader
-              avatar={
-                <Avatar aria-label="Recipe" className={classes.avatar}>
-                  R
-                </Avatar>
-              }
-              action={
-                <IconButton>
-                  <MoreVertIcon />
-                </IconButton>
-              }
-              title="Shrimp and Chorizo Paella"
-              subheader="September 14, 2016"
-            />
-            <CardMedia
-              className={classes.media}
-              image="https://material-ui.com/static/images/cards/paella.jpg"
-              title="Contemplative Reptile"
-            />
-            <CardContent>
-              <Typography component="p">
-                This impressive paella is a perfect party dish and a fun meal to cook together with
-                your guests. Add 1 cup of frozen peas along with the mussels, if you like.
-              </Typography>
-            </CardContent>
-            <CardActions className={classes.actions} disableActionSpacing>
-              <IconButton aria-label="Add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              <IconButton aria-label="Share">
-                <ShareIcon />
-              </IconButton>
-              <IconButton
-                className={classnames(classes.expand, {
-                  [classes.expandOpen]: this.state.expanded,
-                })}
-                onClick={this.handleExpandClick}
-                aria-expanded={this.state.expanded}
-                aria-label="Show more"
-              >
-                <ExpandMoreIcon />
-              </IconButton>
-            </CardActions>
-            <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
-              <CardContent>
-                <Typography paragraph variant="body2">
-                  Method:
-                </Typography>
-                <Typography paragraph>
-                  Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                  minutes.
-                </Typography>
-                <Typography paragraph>
-                  Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                  heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-                </Typography>
-                <Typography paragraph>
-                  Add rice and stir very gently to distribute. Top with artichokes and peppers, and
-                  cook without stirring, until most of the liquid is absorbed, 15 to 18 minutes.
-                </Typography>
-              </CardContent>
-            </Collapse>
-          </Card>
+        <div className={classes.placeholder}/>
+          <Carousel autoplay>
+            <div>
+              <h3 style={contentStyle}>1</h3>
+            </div>
+            <div>
+              <h3 style={contentStyle}>2</h3>
+            </div>
+            <div>
+              <h3 style={contentStyle}>3</h3>
+            </div>
+            <div>
+              <h3 style={contentStyle}>4</h3>
+            </div>
+          </Carousel>
       </div>
     );
   }
