4f101ab95a9564b2a2fbaf3e4cba719f0ce31c71
Devin Dai <wdai@ualberta.ca>
2021-05-14 21:19:41 -0600

    Home Page Carousel working

--- src/ProductHero.js
+++ src/ProductHero.js
@@ -2,22 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from './components/Typography';
-import ProductHeroLayout from './ProductHeroLayout';
 import IntroBackground0 from './Images/TF_Movie_0.jpg'
 import IntroBackground1 from './Images/TF_Movie_1.jpg'
 import IntroBackground2 from './Images/TF_Movie_2.jpg'
 import IntroBackground3 from './Images/TF_Movie_3.jpg'
+import { Carousel } from 'antd';
+import 'antd/dist/antd.css';
+import { styles as toolbarStyles } from './components/Toolbar';
 
-const backgroundImage = [
-  IntroBackground0,
-  IntroBackground1,
-  IntroBackground2,
-  IntroBackground3
-];
-
-/*const backgroundImage = IntroBackground;*/
 
 const styles = (theme) => ({
+  placeholder: toolbarStyles(theme).root,
   background: {
     backgroundImage: `url(${IntroBackground1})`,
     backgroundColor: 'black', 
@@ -38,20 +33,95 @@ const styles = (theme) => ({
   },
 });
 
-function ProductHero(props) {
-  const {classes} = props;
+class ProductHero extends React.Component {
+  render () {
+    const {classes} = this.props;
 
-  return (
-    <ProductHeroLayout backgroundClassName={classes.background}>
-        <img style={{ display: 'none' }} src={backgroundImage} alt="increase priority" />
-        <Typography color="inherit" align="center" variant="h2" marked="center">
-          TF ARKIVE 
-        </Typography>
-        <Typography variant="h5" color="inherit" className={classes.more}>
-          Welcome to The Collection Archive
-        </Typography>
-    </ProductHeroLayout>
-  );
+    const Style1 = {
+      height: '897px',
+      color: '#fff',
+      lineHeight: '160px',
+      textAlign: 'center',
+      backgroundImage: `url(${IntroBackground0})`,
+      backgroundSize: 'cover',
+    };
+
+    const Style2 = {
+      height: '897px',
+      color: '#fff',
+      lineHeight: '160px',
+      textAlign: 'center',
+      backgroundImage: `url(${IntroBackground3})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+    };
+
+    const Style3 = {
+      height: '897px',
+      color: '#fff',
+      lineHeight: '160px',
+      textAlign: 'center',
+      backgroundImage: `url(${IntroBackground1})`,
+      backgroundSize: 'cover',
+    };
+
+    const Style4 = {
+      height: '897px',
+      color: '#fff',
+      lineHeight: '160px',
+      textAlign: 'center',
+      backgroundImage: `url(${IntroBackground2})`,
+      backgroundSize: 'cover',
+    };
+
+    return (
+      <div>
+        <Carousel autoplay effect="fade">
+            <div>
+              <h3 style={Style1}>
+                <Typography color="inherit" align="center" variant="h2" marked="center">
+                  TF ARKIVE 
+                </Typography>
+                <Typography variant="h5" color="inherit" className={classes.more}>
+                  Welcome to The Collection Archive
+                </Typography>
+              </h3>
+            </div>
+            <div>
+              <h3 style={Style2} >
+                <Typography color="inherit" align="center" variant="h2" marked="center">
+                    TF ARKIVE 
+                  </Typography>
+                  <Typography variant="h5" color="inherit" className={classes.more}>
+                    Welcome to The Collection Archive
+                </Typography>
+              </h3>
+            </div>
+            <div>
+              <h3 style={Style3}>
+                <Typography color="inherit" align="center" variant="h2" marked="center">
+                    TF ARKIVE 
+                  </Typography>
+                  <Typography variant="h5" color="inherit" className={classes.more}>
+                    Welcome to The Collection Archive
+                </Typography>
+              </h3>
+            </div>
+            <div>
+              <h3 style={Style4}>
+                <Typography color="inherit" align="center" variant="h2" marked="center">
+                    TF ARKIVE 
+                  </Typography>
+                  <Typography variant="h5" color="inherit" className={classes.more}>
+                    Welcome to The Collection Archive
+                </Typography>
+              </h3>
+            </div>
+          </Carousel>
+      </div>
+    );
+  }
+  
 }
 
 ProductHero.propTypes = {
